c1a1a2d7cabe1e61d394f65dfbfea5f5b87e476f
zFunx <20762882+zFunx@users.noreply.github.com>
2023-05-06 20:21:33 +0530

    Combine both saved and suggested countries

--- src/pages/api/get-cities.js
+++ src/pages/api/get-cities.js
@@ -15,7 +15,7 @@ const db = getFirestore(firebase);
 
 // Internal params
 const maxSuggesttion = 5;
-const exampleResponse = `['Partial match', 'Los Angeles in United States', 'Los Rios in Chile', 'Los Alamos in United States', 'Los Palacios y Villafranca in Spain', 'Los Santos in Panama'].`
+const exampleResponse = `['Partial match', 'Los Angeles in United States', 'Los Rios in Chile', 'Los Alamos in United States', 'Los Palacios y Villafranca in Spain', 'Los Santos in Panama'].`;
 // const exampleResponse = `["Partial match"]`
 
 async function getSavedCitiesByPartialName(partialName) {
@@ -53,6 +53,9 @@ function removeTrailingPeriod(str) {
     return str;
   }
 }
+function formatSavedCities(cities) {
+  return cities.map((city) => `${city.name} in ${city.country}`);
+}
 
 export default async function handler(req, res) {
   const { query } = req.query;
@@ -70,29 +73,33 @@ export default async function handler(req, res) {
       .json({ message: "query should not exceeds 2 words long" });
   }
 
-  const cities = await getSavedCitiesByPartialName(query);
+  const savedCities = await getSavedCitiesByPartialName(query);
 
   if (
-    1 == cities.length &&
-    query.toLocaleLowerCase() == cities[0].name_lowercase
+    1 == savedCities.length &&
+    query.toLocaleLowerCase() == savedCities[0].name_lowercase
   ) {
     // If we get only one saved city
-    return res.status(200).json({ cities });
-  } else if (maxSuggesttion == cities.length) {
+    return res
+      .status(200)
+      .json({ matchType: "Exact match", cities: formatSavedCities(savedCities) });
+  } else if (maxSuggesttion == savedCities.length) {
     // If we get all saved cities
-    return res.status(200).json({ cities });
+    return res
+      .status(200)
+      .json({ matchType: "Partial match", cities: formatSavedCities(savedCities) });
   } else {
     // If we get either zero saved cities or (between 1 and maxSuggestions)
     // TODO: try catch
     // const openaiResponse = await getOpenAiCities({ query });
     const openaiResponse = exampleResponse;
     // console.log('example response', openaiResponse);
-    let cleanedResponse = openaiResponse.replace(/'/g, '"')
-    cleanedResponse = removeTrailingPeriod(cleanedResponse)
+    let cleanedResponse = openaiResponse.replace(/'/g, '"');
+    cleanedResponse = removeTrailingPeriod(cleanedResponse);
     cleanedResponse = JSON.parse(cleanedResponse);
     return res.status(200).json({
-      matchType: cleanedResponse[0],
-      cities: cleanedResponse.slice(1),
+      matchType: 0 == savedCities.length ? cleanedResponse[0] : 'Partial match',
+      cities: [...cleanedResponse.slice(1), ...formatSavedCities(savedCities)],
     });
   }
 
