9043f528090a848323564f69d3932107e0ff4f59
zFunx <20762882+zFunx@users.noreply.github.com>
2023-05-06 18:58:00 +0530

    Add firebase

---
+++ src/lib/init-firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_apiKey,
+  authDomain: process.env.FIREBASE_authDomain,
+  projectId: process.env.FIREBASE_projectId,
+  storageBucket: process.env.FIREBASE_storageBucket,
+  messagingSenderId: process.env.FIREBASE_messagingSenderId,
+  appId: process.env.FIREBASE_appId,
+  measurementI: process.env.FIREBASE_measurementId
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/api/get-cities.js
+++ src/pages/api/get-cities.js
@@ -1,5 +1,70 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import firebase from "@/lib/init-firebase";
+import {
+  collection,
+  endAt,
+  getDocs,
+  getFirestore,
+  limit,
+  orderBy,
+  query,
+  startAt,
+  where,
+} from "firebase/firestore";
+const db = getFirestore(firebase);
+
+// Internal params
+const maxSuggesttion = 5;
+
+async function getSavedCitiesByPartialName(partialName) {
+  const q = query(
+    collection(db, "cities"),
+    orderBy("name_lowercase"),
+    startAt(partialName.toLowerCase()),
+    endAt(partialName.toLowerCase() + "\uf8ff"),
+    where("is_approved", "==", true),
+    limit(maxSuggesttion)
+  );
+  const cities = [];
+
+  try {
+    const docs = await getDocs(q);
+    docs.forEach((doc) => {
+      cities.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+  } catch (e) {
+    console.log(
+      "Somthing went wrong while searching for cities from Firbase",
+      e
+    );
+  }
+
+  return cities;
+}
+
+export default async function handler(req, res) {
+  const { query } = req.query;
+  const cities = await getSavedCitiesByPartialName(query);
+
+  if (
+    1 == cities.length &&
+    query.toLocaleLowerCase() == cities[0].name_lowercase
+  ) {
+    // If we get only one saved city
+    return res.status(200).json({ cities });
+  } else if (maxSuggesttion == cities.length) {
+    // If we get all saved cities
+    return res.status(200).json({ cities });
+  } else {
+    // If we get either zero saved cities or (between 1 and maxSuggestions)
+    // TODO
+    return res.status(200).json({ cities });
+  }
+
+  // Defaut response
+  res.status(200).json({ cities: [] });
 }
